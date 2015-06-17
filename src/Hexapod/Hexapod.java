ff6dedc3e3355b9889b51f1ca118ea5b4b4aa2a4
Dan Royer <dan@marginallyclever.com>
2015-06-18 00:18:27 +0300

    update Maven pom.xml
    project should now download dependencies automatically.

--- src/Hexapod/Hexapod.java
+++ src/Hexapod/Hexapod.java
@@ -2,7 +2,6 @@ package Hexapod;
 
 
 import java.awt.BorderLayout;
-import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
